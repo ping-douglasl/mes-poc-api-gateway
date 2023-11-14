d9b635f184a76fb0f6300c9ecb038247e58d04a3
Douglas Linsmeyer <douglinsmeyer@gmail.com>
2023-11-13 20:28:35 -0700

    Configures initial setup.

---
+++ clean.sh
@@ -0,0 +1,3 @@
+kubectl delete -f https://github.com/nginxinc/nginx-gateway-fabric/releases/download/v1.0.0/nginx-gateway.yaml
+kubectl delete -f https://github.com/nginxinc/nginx-gateway-fabric/releases/download/v1.0.0/crds.yaml
+kubectl delete -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v0.8.1/standard-install.yaml
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v0.8.1/standard-install.yaml
+kubectl apply -f https://github.com/nginxinc/nginx-gateway-fabric/releases/download/v1.0.0/crds.yaml
+kubectl apply -f https://github.com/nginxinc/nginx-gateway-fabric/releases/download/v1.0.0/nginx-gateway.yaml
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/nginx-gateway-fabric/v1.0.0/deploy/manifests/service/loadbalancer.yaml
+
 CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Deploy the app to k8s
