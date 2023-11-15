67b990fae0ec20c4d916130de5f69e63d17a210d
Douglas Linsmeyer <douglasl@ping.com>
2023-11-15 12:45:25 -0700

    Adds custom manifests to clean up.

--- clean.sh
+++ clean.sh
@@ -1,3 +1,7 @@
 kubectl delete -f https://github.com/nginxinc/nginx-gateway-fabric/releases/download/v1.0.0/nginx-gateway.yaml
 kubectl delete -f https://github.com/nginxinc/nginx-gateway-fabric/releases/download/v1.0.0/crds.yaml
 kubectl delete -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v0.8.1/standard-install.yaml
+
+CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+kubectl delete --filename="$CURRENT_DIR/manifests" --overwrite
